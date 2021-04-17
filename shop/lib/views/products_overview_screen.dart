602142c0170679adbe4abf2af478d02a8c555216
Your Namelucashr <lucas_rdasilva@hotmail.com>
2021-04-17 11:36:41 -0300

    170421

---
+++ shop/lib/views/products_overview_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../data/dummy_data.dart';
+import '../models/product.dart';
+import '../widgets/product_item.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProduts = DUMMY_PRODUCTS;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProduts.length,
+        itemBuilder: (ctx, i) => ProductItem(loadedProduts[i]),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
+  }
+}
